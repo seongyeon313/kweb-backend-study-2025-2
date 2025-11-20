6311505db8a81774c6d1e166a4d289a373ccb98a
seongyeon <dkstjddus313@gmail.com>
2025-11-20 22:13:32 +0900

    week5

--- week5/app.js
+++ week5/app.js
@@ -2,6 +2,7 @@ const {runQuery} = require('./database');
 
 const express = require('express');
 const session = require('express-session');
+const sanitizeHTML = require('sanitize-html');
 
 const app = express();
 
@@ -9,18 +10,49 @@ app.use(session({
 	secret: 'asdf',
 	resave: false,
 	saveUninitialized: true,
-});
+}));
 
 app.get('/me', (req, res) => {
-
+	const {requester} = req.session;
+	if (!requester) return res.send('You are not logged in');
+	const {id, name, status} = requester;
+	return res.send(`id: ${id}, name: ${name}, status: ${status}`);
 });
 
 app.get('/login', (req, res) => {
+	const username = req.query.username;
+	const password = req.query.password;
 
+	if (password !== '1q2w3e4r'){
+		res.send('Wrong password');
+		return;
+	}
+
+	req.session.requester = {
+		id : 1,
+		name : username,
+		status : '졸림'
+	}
+
+	return res.send('login successful');
 });
 
 app.get('/logout', (req, res) => {
+	req.session.destroy(err =>{
+		if(err) return res.send('Something went wrong');
+		return  res.send('logout successful');
+	});
+});
 
+app.get('/student', async (req, res) => {
+	const query = req.query.id;
+	const result = await runQuery('select * from students where student_id=?', [query]);
+	return res.send(result);
+});
+
+app.get('/xss', (req, res) => {
+	const content = req.query.content;
+	return res.send(sanitizeHTML(content));
 });
 
 app.listen(3000, ()=>console.log('server started!'));
--- week5/database.js
+++ week5/database.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2/promise');
-
+/*
 const pool = mysql.createPool({
     host: 'db',
     port: 3306,
@@ -7,11 +7,20 @@ const pool = mysql.createPool({
     database: 'kweb_db',
     password: '1q2w3e4r'
 });
+*/
+//로컬
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'kweb',
+    database: 'kweb_db',
+    password: '0313'
+});
 
-
-const runQuery = async sql => {
+const runQuery = async (sql, data) => {
     const conn = await pool.getConnection();
     try {
+        const psql = conn.format(sql, data);
         const [result] = await conn.query(sql);
         return result;
     } finally {
